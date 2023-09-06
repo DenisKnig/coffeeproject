e613452d73b9fb341f84115f9695633de674b634
DenisKnig <denis.knig.95@gmail.com>
2023-09-06 21:24:09 +0300

    ДЗ 7. Final Project: Content

---
+++ views/parts/home/banner.php
@@ -0,0 +1,11 @@
+<section id="banner">
+    <div class="container banner-container">
+        <div class="row">
+            <div class="col-12 col-md-9 col-xl-6 banner-content">
+                <p class="banner-p">NOW YOU CAN FEEL THE ENERGY</p>
+                <h3 class="banner-h3">Start your day with <br> a black Coffee</h3>
+                <a href="#" class="btn btn-outline-primary banner-button">BUY NOW</a>
+            </div>
+        </div>
+    </div>
+</section>
