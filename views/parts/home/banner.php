3e00a57035972391597936d955cafc47270a5851
DenisKnig <denis.knig.95@gmail.com>
2023-09-12 00:20:37 +0300

    здаю в такому вигляді, чомусь сайт не грузить

--- views/parts/home/banner.php
+++ views/parts/home/banner.php
@@ -1,10 +1,14 @@
 <section id="banner">
-    <div class="container banner-container">
+    <div class="container">
         <div class="row">
             <div class="col-12 col-md-9 col-xl-6 banner-content">
-                <p class="banner-p">NOW YOU CAN FEEL THE ENERGY</p>
-                <h3 class="banner-h3">Start your day with <br> a black Coffee</h3>
-                <a href="#" class="btn btn-outline-primary banner-button">BUY NOW</a>
+                <p><?= $content['banner']['title'] ?? '' ?></p>
+                <h3><?= $content['banner']['subtitle'] ?? '' ?></h3>
+                <?php if ($content['banner']['link']): ?>
+                    <a href="<?= $content['banner']['link']['href'] ?>"
+                       class="btn btn-outline-primary banner-button"
+                    ><?= $content['banner']['link']['title'] ?></a>
+                <?php endif ?>
             </div>
         </div>
     </div>
